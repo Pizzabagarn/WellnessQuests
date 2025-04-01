ea23b2d67002e94b807fad9729d7cb150fdd120f
AlexanderWestman <superalle96@gmail.com>
2025-04-02 01:40:53 +0200

    Inför demo_Retrospekt

--- app/src/main/java/com/example/wellnessquest/QuestActivity.java
+++ app/src/main/java/com/example/wellnessquest/QuestActivity.java
@@ -3,7 +3,6 @@ package com.example.wellnessquest;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,34 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class QuestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_quest);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 
-            //Refererar till ikonerna i activity_main.xml
+            ImageView demoQuestImg = findViewById(R.id.imgDemoQuest);
 
-            ImageView iconQuests = findViewById(R.id.imgQuestsIcon);
-            ImageView iconMap = findViewById(R.id.imgMapIcon);
-
-            iconQuests.setOnClickListener(view -> {
-                Intent intent = new Intent(MainActivity.this, QuestActivity.class);
+            demoQuestImg.setOnClickListener(view -> {
+                Intent intent = new Intent(QuestActivity.this, QuestTextActivity.class);
                 startActivity(intent);
             });
 
-            iconMap.setOnClickListener(view -> {
-                Intent intent = new Intent(MainActivity.this, MapActivity.class);
-                startActivity(intent);
-            });
             return insets;
-
         });
     }
 }
