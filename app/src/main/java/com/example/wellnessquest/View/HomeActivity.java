4fd8b53a3196153d2f12d960d760677f34242544
lowisacs <lowisasvensson@gmail.com>
2025-04-17 11:44:12 +0200

    uppdaterat homeactivity, lagt till drawer_menu, och uppdaterat activity_home
    Lagt till navigation drawer

--- app/src/main/java/com/example/wellnessquest/View/HomeActivity.java
+++ app/src/main/java/com/example/wellnessquest/View/HomeActivity.java
@@ -1,28 +1,73 @@
 package com.example.wellnessquest.View;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.wellnessquest.R;
+import com.example.wellnessquest.databinding.ActivityHomeBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
+    private ActivityHomeBinding binding;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());        setContentView(R.layout.activity_home);
+        setContentView(binding.getRoot());
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+        setSupportActionBar(binding.toolbar);
 
-            return insets;
+        toggle = new ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        binding.drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Hantera klick i navigation drawer
+        binding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                // Just nu: visa bara logg eller gör toast
+                if (id == R.id.nav_home) {
+                    showToast("Home selected");
+                } else if (id == R.id.nav_quests) {
+                    showToast("Quests selected");
+                } else if (id == R.id.nav_map) {
+                    showToast("Map selected");
+                } else if (id == R.id.nav_profile) {
+                    showToast("Profile selected");
+                }
+
+                binding.drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
         });
     }
+
+    private void showToast(String msg) {
+        android.widget.Toast.makeText(this, msg, android.widget.Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
+
+
