b8db2e625f8a78f4a719b46bf7ed6a31c461ac72
AlexanderWestman <superalle96@gmail.com>
2025-04-20 01:02:19 +0200

    Fixade till Quest.java efter merge med questlogic-User/login logic

--- app/src/main/java/com/example/wellnessquest/model/Quest.java
+++ app/src/main/java/com/example/wellnessquest/model/Quest.java
@@ -1,4 +1,69 @@
 package com.example.wellnessquest.model;
 
 public class Quest {
+    private String id;
+    private String title;
+    private String description;
+    private String category; // "Fitness" eller "Mind"
+    private boolean isCompleted;
+    private int coins;
+
+    public Quest(String id, String title, String description, String category, boolean isCompleted, int coins) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.isCompleted = isCompleted;
+        this.coins = coins;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    // Setters
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
 }
