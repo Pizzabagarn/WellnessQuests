8fa14dfa4f2bc365737aeb3b40fd7bc162108de3
lowisacs <lowisasvensson@gmail.com>
2025-05-15 13:27:36 +0200

    Update Level.java

--- app/src/main/java/com/example/wellnessquest/model/Level.java
+++ app/src/main/java/com/example/wellnessquest/model/Level.java
@@ -1,5 +1,20 @@
 package com.example.wellnessquest.model;
 
 public class Level {
-   
+
+    public int levelNumber;
+    public int cost;
+
+    public Level (int levelNumber, int cost){
+        this.levelNumber = levelNumber;
+        this.cost = cost;
+    }
+
+    public int getLevelNumber() {
+        return levelNumber;
+    }
+
+    public int getCost() {
+        return cost;
+    }
 }
